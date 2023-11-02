72686db9ef9e8f2f2f868b70589b657618dc11f1
leastyeast <Katherine.Kisiloski@edu.sait.ca>
2023-11-02 16:29:06 -0600

    step 1

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-file = open("three_digit_numbers.txt")
+f = open("three_digit_numbers.txt") 
+sample = f.read()
+cols = sample.split()
+num_list = []
+for col in cols:
+    num = int(col)
+    num_list.append(num)
 
-print(file.read())
-
-x = sorted(file)
-
-print(x)
+#print(cols)
+#print(num_list)
+start = min(num_list)
+for count in range(start, max(num_list)):
+    # print(count)
+    if count not in num_list:
+        print(f"{count} is missing")
